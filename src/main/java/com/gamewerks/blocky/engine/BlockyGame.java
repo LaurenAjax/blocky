b1ca461b518df99c74254a056b837376be158a7a
ajaxlaur <ajaxlaur@132.161.196.207>
2025-02-09 16:28:39 -0600

    Undid reversed gravity to work out bugs.

--- src/main/java/com/gamewerks/blocky/engine/BlockyGame.java
+++ src/main/java/com/gamewerks/blocky/engine/BlockyGame.java
@@ -2,6 +2,7 @@ package com.gamewerks.blocky.engine;
 
 import com.gamewerks.blocky.util.Constants;
 import com.gamewerks.blocky.util.Position;
+import java.util.Random;
 
 public class BlockyGame {
     private static final int LOCK_DELAY_LIMIT = 30;
@@ -11,6 +12,8 @@ public class BlockyGame {
     private Direction movement;
     
     private int lockCounter;
+    private int currentPiece = 0;
+    private PieceKind[] pieceArray = shuffle(PieceKind.ALL);
     
     public BlockyGame() {
         board = new Board();
@@ -19,11 +22,32 @@ public class BlockyGame {
         trySpawnBlock();
     }
     
+    private PieceKind[] shuffle(PieceKind[] pieces) {
+        int arr[] = {0, 1, 2, 3, 4, 5, 6};
+        PieceKind[] newPieces = new PieceKind[7];
+        Random rand = new Random();
+        for (int i = 0; i < 7; i++) {
+            int j = rand.nextInt(7);
+            if (arr[j] == -1) {
+                i--;
+            } else {
+                newPieces[i] = pieces[arr[j]];
+                arr[j] = -1;
+            }
+        }
+        return newPieces;
+    }
+    
     private void trySpawnBlock() {
         if (activePiece == null) {
-            activePiece = new Piece(PieceKind.I, new Position(Constants.BOARD_HEIGHT - 20, Constants.BOARD_WIDTH / 2 - 2));
+            activePiece = new Piece(pieceArray[currentPiece], new Position(Constants.BOARD_HEIGHT - 1, Constants.BOARD_WIDTH / 2 - 2));
             if (board.collides(activePiece)) {
                 System.exit(0);
+            }
+            currentPiece++;
+            if (currentPiece >= 7) {
+                pieceArray = shuffle(PieceKind.ALL);
+                currentPiece = 0;
             }
         }
     }
@@ -43,14 +67,14 @@ public class BlockyGame {
         default:
             throw new IllegalStateException("Unrecognized direction: " + movement.name());
         }
-        if (!board.collides(activePiece)) {
+        if (!board.collides(activePiece.getLayout(), nextPos)) {
             activePiece.moveTo(nextPos);
         }
     }
     
     private void processGravity() {
-        Position nextPos = activePiece.getPosition().add(1, 0);
-        if (!board.collides(activePiece)) {
+        Position nextPos = activePiece.getPosition().add(-1, 0);
+        if (!board.collides(activePiece.getLayout(), nextPos)) {
             lockCounter = 0;
             activePiece.moveTo(nextPos);
         } else {
